bd109fa67cb787408e9f96e674e7c48a92f65d72
DonggunLim <Ldonggun6766@gmail.com>
2024-11-09 16:56:46 +0900

    add a tag into education title

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -11,7 +11,11 @@ export default function Education() {
             <img src={universityLogo} alt="university-logo" />
           </div>
           <div className="education-description">
-            <p>한남대학교 (Hannam University)</p>
+            <p>
+              <a href="https://www.hannam.ac.kr/kor/main/" target="_blank">
+                한남대학교 (Hannam University)
+              </a>
+            </p>
             <p>Public Administration 중퇴</p>
             <p>2015년 - 2018년</p>
           </div>
