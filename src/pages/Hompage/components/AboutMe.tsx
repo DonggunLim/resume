054a0ef68a903f1b634cc71611234d2e94feb43e
DonggunLim <Ldonggun6766@gmail.com>
2025-02-04 19:48:33 +0900

    feat: activity accordion 수정

--- src/pages/Hompage/components/AboutMe.tsx
+++ src/pages/Hompage/components/AboutMe.tsx
@@ -1,34 +1,28 @@
-const Aboutme = () => {
+const AboutMe = () => {
   return (
-    <article className="flex flex-col gap-y-8 leading-loose">
+    <article className="flex flex-col gap-y-6 leading-relaxed">
       <p>
-        안녕하세요! 웹 개발자를 목표로 꾸준히 배워가고 있는 임동건입니다.
-        유튜브를 통해 개발에 호기심을 가지게 되었고, 직접 웹을 만들어보는 과정이
-        흥미로워 본격적으로 공부를 시작했습니다. 웹의
+        안녕하세요. 웹 개발자 임동건입니다. 개발에 관심을 가지게 된 후, 웹을
+        직접 만들어보는 과정에서
         <span className="text-primary-light-text dark:text-primary-dark-text font-semibold">
-          시각적인 부분을 만들면서 내부 로직을 설계하는 과정
+          UI 구성과 내부 로직 설계
         </span>
-        이 저의 호기심을 자극했습니다.
+        에 큰 매력을 느끼고 본격적으로 공부를 시작했습니다.
       </p>
       <p>
-        현재 저는 엘리스 트랙이라는 부트 캠프 과정을 수료 중입니다. 경험 많으신
-        코치님들로부터 많은 것을 배우고, 다른 수강생들과 의견을 나누는 경험이
-        시야를 넓히는데 큰 도움이 되고 있습니다. 최근에는
+        현재
         <span className="text-primary-light-text dark:text-primary-dark-text font-semibold">
-          React로 개인 UI 라이브러리를 개발하고 배포하며, 컴포넌트 설계와 배포를
-          하는 과정
+          React 기반의 UI 라이브러리를 개발 및 배포
         </span>
-        을 경험 해 볼 수 있었습니다. 큰 흥미를 느끼고 잇는 프로젝트 입니다. 매일
-        많은 시간을 투자할 수는 없겠지만 지속적으로 업데이트해 볼 계획입니다.
+        하며, 컴포넌트 설계를 경험하고 있습니다. 꾸준히 개선하여 더 나은 사용자
+        경험을 만드는 데 집중하고 있습니다.
       </p>
       <p>
-        여가 시간에는 오래 즐겨온 취미인 헬스를 꾸준히 하고 있습니다. 개발자로서
-        책상에 앉아 있는 시간이 많아지면서 건강에 더욱 신경을 쓰고 있는데, 점점
-        목,허리가 아픈 일이 많아지면서 걱정입니다.😂 이 글을 읽고 계신
-        여러분들은 자세를 잘 유지하시고 건강을 챙기셨으면 좋겠네요!😁
+        여가 시간에는 헬스를 즐기며 건강을 관리합니다. 개발과 건강의 균형을
+        유지하며, 꾸준히 성장해 나가고자 합니다. 😊
       </p>
     </article>
   );
 };
 
-export default Aboutme;
+export default AboutMe;
--- src/pages/Hompage/components/LatestActivity.tsx
+++ src/pages/Hompage/components/LatestActivity.tsx
@@ -1,49 +1,62 @@
 import { Accordion } from "@imdonggun/react-ui-kit";
 import eliceBrandLogo from "../../../assets/elice-track-logo.svg";
 import { FaLink } from "react-icons/fa6";
-
+import { RiArrowDownWideFill } from "react-icons/ri";
 export default function LatestActivity() {
   return (
     <section>
       <h1 className="text-xl font-bold text-primary-light-text dark:text-primary-dark-text pb-3">
-        최근 활동
+        Activity
       </h1>
-      <ul>
-        <li
+      <Accordion>
+        <div
           className="group/item rounded-md p-4 
             hover:bg-slate-400/10
            dark:hover:bg-slate-700/20
            relative
            "
         >
-          <div className="flex gap-x-2">
-            <div className="flex justify-center dark:bg-gray-200/90 p-3 rounded-md shadow-md">
-              <img
-                src={eliceBrandLogo}
-                alt="elice-track-logo"
-                className="object-contain w-16 h-16"
-              />
-            </div>
-            <div className="">
-              <p
-                className="text-lg font-semibold pb-2 
-            group-hover/item:text-primary-light-text
-            dark:group-hover/item:text-primary-dark-text"
-              >
-                엘리스 트랙
-              </p>
-              <div className="text-sm font-medium">
-                <p>수강중</p>
-                <p> 2024년 10월 - 현재</p>
+          <Accordion.Button className="w-full">
+            <Accordion.Title>
+              <div className="flex gap-x-2 w-full">
+                <div className="flex justify-center dark:bg-gray-200/90 p-3 rounded-md shadow-md">
+                  <img
+                    src={eliceBrandLogo}
+                    alt="elice-track-logo"
+                    className="object-contain w-16 h-16"
+                  />
+                </div>
+                <div className="">
+                  <p
+                    className="text-lg font-semibold pb-2 
+                group-hover/item:text-primary-light-text
+                dark:group-hover/item:text-primary-dark-text"
+                  >
+                    엘리스 트랙
+                  </p>
+                  <div className="text-sm font-medium">
+                    <p>수강중</p>
+                    <p> 2024년 10월 - 현재</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <Accordion>
+            </Accordion.Title>
+            <RiArrowDownWideFill className="w-8 h-8 absolute top-10 right-5 opacity-0 group-hover/item:opacity-100 transition-opacity duration-200" />
+          </Accordion.Button>
+          <Accordion.Content>
             <ul className="py-8 space-y-2">
-              <li className="flex items-start gap-2 text-sm font-medium">
-                <p>
-                  7주차 : <span className="text-blue-300">React </span>
-                  상태관리 Context API · Redux-toolkit · Recoil · Zustand
+              <li
+                className="flex items-center gap-2 text-sm font-semibold
+               group-hover/item:text-primary-light-text
+                dark:group-hover/item:text-primary-dark-text
+                cursor-pointer
+                "
+              >
+                <span>
+                  <FaLink />
+                </span>
+                <p className="flex items-center">
+                  3차 프로젝트 (Full-stack react + express)
                 </p>
               </li>
               <li
@@ -57,52 +70,107 @@ export default function LatestActivity() {
                   <FaLink />
                 </span>
                 <p className="flex items-center">
-                  @imdonggun/react-ui-kit npm package 배포
+                  vite-plugin-generate-todo (vite plugin project)
+                </p>
+              </li>
+              <li
+                className="flex items-center gap-2 text-sm font-semibold
+               group-hover/item:text-primary-light-text
+                dark:group-hover/item:text-primary-dark-text
+                cursor-pointer
+                "
+              >
+                <span>
+                  <FaLink />
+                </span>
+                <p className="flex items-center">
+                  Vite + SEO + SSR + FileBase Route (vite boiler project)
                 </p>
               </li>
               <li className="flex items-start gap-2 text-sm font-medium">
                 <p>
-                  6주차 : <span className="text-blue-300">React </span>
-                  스타일링 · SPA · react-router-dom
+                  16~18주차 : react 실전 라이브러리 · Tanstack-query · Docker ·
+                  ci/cd · gitLab
+                </p>
+              </li>
+              <li
+                className="flex items-center gap-2 text-sm font-semibold
+               group-hover/item:text-primary-light-text
+                dark:group-hover/item:text-primary-dark-text
+                cursor-pointer
+                "
+              >
+                <span>
+                  <FaLink />
+                </span>
+                <p className="flex items-center">
+                  2차 프로젝트 (Full-stack react + express)
+                </p>
+              </li>
+              <li
+                className="flex items-center gap-2 text-sm font-semibold
+               group-hover/item:text-primary-light-text
+                dark:group-hover/item:text-primary-dark-text
+                cursor-pointer
+                "
+              >
+                <span>
+                  <FaLink />
+                </span>
+                <p className="flex items-center">
+                  숙박앱 미니 프로젝트 (Full-stack react + express)
                 </p>
               </li>
               <li className="flex items-start gap-2 text-sm font-medium">
                 <p>
-                  5주차 : <span className="text-blue-300">React · </span>
-                  <span className="text-blue-500">TypeScript</span> · React
-                  Hooks 기초 · React Hooks 심화
+                  11~13주차 : DataBase 기초 (mongoDB · mongoose) Node.js ·
+                  Express
+                </p>
+              </li>
+              <li
+                className="flex items-center gap-2 text-sm font-semibold
+               group-hover/item:text-primary-light-text
+                dark:group-hover/item:text-primary-dark-text
+                cursor-pointer
+                "
+              >
+                <span>
+                  <FaLink />
+                </span>
+                <p className="flex items-center">
+                  1차 프로젝트 (Backend express + mongoDB)
                 </p>
               </li>
               <li className="flex items-start gap-2 text-sm font-medium">
                 <p>
-                  4주차 : <span className="text-blue-300">React </span>개요 JSX
-                  Props State 이벤트 처리 ·
-                  <span className="text-blue-500"> TypeScript</span> 기초 심화
+                  7~8주차 : React 상태관리 Context API · Redux-toolkit · Recoil
+                  · Zustand · FireBase
+                </p>
+              </li>
+              <li
+                className="flex items-center gap-2 text-sm font-semibold
+               group-hover/item:text-primary-light-text
+                dark:group-hover/item:text-primary-dark-text
+                cursor-pointer
+                "
+              >
+                <span>
+                  <FaLink />
+                </span>
+                <p className="flex items-center">
+                  @imdonggun/react-ui-kit lib 배포
                 </p>
               </li>
               <li className="flex items-start gap-2 text-sm font-medium">
                 <p>
-                  3주차 : <span className="text-yellow-400">JavaScript </span>
-                  이벤트 루프 Promise · async/await 예외 처리 · Ajax, RestAPI
-                </p>
-              </li>
-              <li className="flex items-start gap-2 text-sm font-medium">
-                <p>
-                  2주차 : <span className="text-yellow-400">JavaScript</span>{" "}
-                  문법 · DOM 이벤트 · ES6 배열 메서드 · 실행 컨텍스트 this
-                </p>
-              </li>
-              <li className="flex items-start gap-2 text-sm font-medium">
-                <p>
-                  1주차 : <span className="text-red-400">HTML</span> ·
-                  <span className="text-yellow-400">CSS</span> · Layout · 반응형
-                  웹사이트 · Git 기초
+                  1~6주차 : React, TypeScript 기본 학습 (컴포넌트, 상태관리,
+                  스타일링, React Router)
                 </p>
               </li>
             </ul>
-          </Accordion>
-        </li>
-      </ul>
+          </Accordion.Content>
+        </div>
+      </Accordion>
     </section>
   );
 }
