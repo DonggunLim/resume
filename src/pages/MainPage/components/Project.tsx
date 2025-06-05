acf2aad37b5cdc92680db115be7f2d8583f3fcb0
DonggunLim <Ldonggun6766@gmail.com>
2025-06-05 11:52:26 +0900

    style: project ui 수정

--- src/pages/MainPage/components/Project.tsx
+++ src/pages/MainPage/components/Project.tsx
@@ -1,48 +1,43 @@
-import { teamProjects } from "../../../consts/projects";
+import { PROJECTS } from "../../../consts/projects";
+import { SquareArrowOutUpRight } from "lucide-react";
+import { Users } from "lucide-react";
 
 const Project = () => {
   return (
     <section
       id="project"
-      className="mx-auto flex min-h-screen max-w-5xl flex-col items-start justify-start px-6 py-16"
+      className="mx-auto flex min-h-screen w-fit max-w-5xl flex-col items-center justify-center"
     >
-      <ul className="w-full space-y-12">
-        <h2 className="text-base font-bold">Projects</h2>
-        {teamProjects.map((project) => (
-          <li key={project.title}>
+      <ul className="w-full space-y-8">
+        {PROJECTS.map((project) => (
+          <li key={project.title} className="group relative cursor-pointer">
             <div className="my-4 flex items-center gap-x-2">
               <img
                 src={project.icon}
                 decoding="async"
                 loading="lazy"
-                className="h-8 w-8 rounded-sm"
+                className="h-8 w-8 rounded-sm object-contain"
               />
-              <h3 className="text-base font-semibold">{project.title}</h3>
+              <h3 className="text-base font-semibold">{project.title} </h3>
+              <div className="group-hover:text-highlight ml-2 flex items-center gap-1 text-sm text-gray-400">
+                <Users className="h-4 w-4" />
+                <span>{project.type}</span>
+              </div>
             </div>
             <p className="my-4 text-white">{project.description}</p>
-            <p className="text-gray-400">{project.techStack}</p>
-            <div className="my-4 flex flex-wrap gap-4">
-              <a
-                href={project.web}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-primary/80 text-gray-300 underline"
-                aria-label="Live Website Link"
-                title="Live Website"
-              >
-                {/* <LiveIcon /> */}
-              </a>
-              <a
-                href={project.repoFront}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-primary/80 text-gray-300 underline"
-                aria-label="Github Link"
-                title="Github"
-              >
-                {/* <GithubIcon /> */}
-              </a>
-            </div>
+            {project.techStack && (
+              <ul className="mb-4 flex flex-wrap gap-2 text-sm text-gray-300">
+                {project.techStack.map((tech) => (
+                  <li
+                    key={tech}
+                    className="group-hover:text-highlight rounded bg-gray-800 px-2 py-1 text-xs text-white"
+                  >
+                    {tech}
+                  </li>
+                ))}
+              </ul>
+            )}
+            <SquareArrowOutUpRight className="group-hover:text-highlight absolute top-0 right-0" />
           </li>
         ))}
       </ul>
