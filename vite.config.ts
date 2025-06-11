51b300f4256158990ee6282b94298e8611d3b56a
DonggunLim <Ldonggun6766@gmail.com>
2025-06-11 10:36:01 +0900

    style: code 스타일 적용

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import mdx from "@mdx-js/rollup";
+import rehypeHighlight from "rehype-highlight";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), mdx()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    mdx({
+      rehypePlugins: [rehypeHighlight],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
